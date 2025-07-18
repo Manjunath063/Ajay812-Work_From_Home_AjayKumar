c19a106f7ee39dfc6c1772eb8a7e2d51b3f2db43
Manjunath063 <manjunathjangam198@gmail.com>
2025-07-18 10:13:40 +0530

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -19,14 +19,14 @@ const pool = new Pool({
 app.use(cors({
   origin: [
     process.env.FRONTEND_URL,
-    "http://16.171.250.116:3072",
+    "http://51.21.200.32:3072",
     "http://127.0.0.1:5500",
-    "http://16.171.250.116:5500",
+    "http://51.21.200.32:5500",
     "http://127.0.0.1:5501",
     "http://127.0.0.1:5503", // Added to allow requests from this origin
-    "http://16.171.250.116:5503", // Added to allow requests from this origin
-    "http://16.171.250.116:8132",
-    "http://16.171.250.116:8133"
+    "http://51.21.200.32:5503", // Added to allow requests from this origin
+    "http://51.21.200.32:8132",
+    "http://51.21.200.32:8133"
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
@@ -202,5 +202,5 @@ app.get('/hr', (req, res) => {
 
 // Start server
 app.listen(port, () => {
-  console.log(`Server running on http://16.171.250.116:${port}`);
+  console.log(`Server running on http://51.21.200.32:${port}`);
 });
